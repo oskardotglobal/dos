ce757f41176b9416e26351882536b27e2dd4fc55
karl <github@karl.my.id>
2024-05-08 11:18:02 +0200

    Feat: Main Menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'menu-gradient-end': '#6E7788',
+        'menu-gradient-transition': '#A2ACBD',
+        'menu-gradient-start': '#012A4A',
+        'primary-button': '#3E4756',
+        'primary-button-hover': '#465060',
+        'secondary-button': '#556176',
+        'secondary-button-hover': '#5b687f'
+      },
+      fontFamily: {
+        'majorMonoDisplay': ['MajorMonoDisplay-Regular.ttf', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
