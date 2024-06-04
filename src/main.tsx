e8c4499418ae73f87a7c1a4a971f9f9c749c94cc
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-06-04 12:22:22 +0200

    fix: remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Client} from "boardgame.io/react";
-import {Local} from "boardgame.io/multiplayer";
-
-import {DosGame} from "$/lib/game";
-import Board from "$/components/Board";
-import LoadingScreen from "$/components/LoadingScreen";
 
 import "$/styles/globals.css";
 import Menu from "$/components/Menu";
