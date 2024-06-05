ac37c13ea26e7bc51da286ebda173bafac541033
Karl Wünsche <snaeker58@users.noreply.github.com>
2024-06-05 11:31:29 +0200

    Feat: More Cards on Deck

--- src/components/board/Background.tsx
+++ src/components/board/Background.tsx
@@ -5,14 +5,14 @@ export default function Background() {
             <div style={{position: 'absolute', top: '560px', left: '316px', width: '808px', height: '215px'}}
                  className="rounded-lg bg-player-deck opacity-20"></div>
 
-            <div style={{position: 'absolute', top: '30px', left: '420px', width: '600px', height: '170px'}}
+            <div style={{position: 'absolute', top: '30px', left: '420px', width: '598px', height: '171px'}}
                  className="rounded-lg bg-enemy-deck opacity-15"></div>
-            <div style={{position: 'absolute', top: '110px', left: '1170px', width: '170px', height: '600px'}}
+            <div style={{position: 'absolute', top: '109px', left: '1170px', width: '170px', height: '597px'}}
                  className="rounded-lg bg-enemy-deck opacity-15"></div>
-            <div style={{position: 'absolute', top: '110px', left: '100px', width: '170px', height: '600px'}}
+            <div style={{position: 'absolute', top: '109px', left: '100px', width: '170px', height: '597px'}}
                  className="rounded-lg bg-enemy-deck opacity-15"></div>
 
-            <div style={{position: 'absolute', top: '255px', left: '545px', width: '350px', height: '250px'}}
+            <div style={{position: 'absolute', top: '273px', left: '563px', width: '314px', height: '215px'}}
                  className="rounded-lg bg-enemy-deck opacity-15"></div>
         </div>
     );
