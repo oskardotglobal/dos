e087db5f00cdcab76fbd15f82cd360d76d270d4d
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-06-18 10:51:45 +0200

    fix: split hand and enemy hands into different components

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -5,9 +5,23 @@ import Background from "$/components/board/Background";
 import Hand from "$/components/board/Hand";
 import DiscardPile from "$/components/board/DiscardPile";
 import Deck from "$/components/board/Deck";
+import {assert} from "$/lib/util/assertions";
+import Enemy from "$/components/board/Enemy";
 
 export default function Board(props: BoardProps<SerializableGameState>) {
     const G = useMemo(() => GameState.deserialize(props.G), [props.G]);
+
+    assert(props.playerID !== null, "PlayerID should never be null");
+
+    const otherPlayers = useMemo(() => {
+        // this assertion has to run twice due to changed scope
+        assert(props.playerID !== null, "PlayerID should never be null");
+
+        const allPlayers = props.ctx.playOrder;
+        allPlayers.splice(allPlayers.indexOf(props.playerID))
+
+        return allPlayers;
+    }, [props.playerID, props.ctx.playOrder]);
 
     return <main>
         <Background/>
@@ -15,11 +29,15 @@ export default function Board(props: BoardProps<SerializableGameState>) {
         <DiscardPile discardPile={G.discardPile}/>
         <Deck moves={props.moves}/>
 
+        <Hand
+            player={G.getPlayer(props.playerID)}
+            moves={props.moves}
+        />
+
         {
-            props.ctx.playOrder.map(
-                (id, i) => <Hand
+            otherPlayers.map(
+                (id, i) => <Enemy
                     player={G.getPlayer(id)}
-                    moves={props.moves}
                     playerIndex={i}
                     key={i}
                 />
--- src/components/board/Hand.tsx
+++ src/components/board/Hand.tsx
@@ -11,60 +11,25 @@ import {useMemo} from "react";
  *
  * Should a card be a wish card, the component renders a set of buttons allowing the player to choose the wished color.
  *
- * @constructor
+ * @component
  */
 export default function Hand(props: {
     player: Player,
-    playerIndex: number,
     moves: Record<string, (...args: any[]) => void>
 }) {
     const positions = [
-        [
-            [321, 565],
-            [381, 565],
-            [441, 565],
-            [501, 565],
-            [561, 565],
-            [621, 565],
-            [681, 565],
-            [741, 565],
-            [801, 565],
-            [861, 565],
-            [921, 565],
-            [981, 565]
-        ],
-        [
-            [1175, 114],
-            [1175, 176],
-            [1175, 236],
-            [1175, 296],
-            [1175, 356],
-            [1175, 410],
-            [1175, 476],
-            [1175, 536],
-            [1175, 596]
-        ],
-        [
-            [125, 114],
-            [125, 176],
-            [125, 236],
-            [125, 296],
-            [125, 356],
-            [125, 410],
-            [125, 476],
-            [125, 741],
-        ],
-        [
-            [424, 34],
-            [485, 34],
-            [545, 34],
-            [605, 34],
-            [665, 34],
-            [725, 34],
-            [785, 34],
-            [845, 34],
-            [905, 34]
-        ]
+        [321, 565],
+        [381, 565],
+        [441, 565],
+        [501, 565],
+        [561, 565],
+        [621, 565],
+        [681, 565],
+        [741, 565],
+        [801, 565],
+        [861, 565],
+        [921, 565],
+        [981, 565]
     ];
 
     const hand = useMemo(() => props.player.getHand(), [props.player]);
@@ -72,13 +37,13 @@ export default function Hand(props: {
     return <>
         {
             hand
-                .slice(0, Math.min(positions[props.playerIndex].length, hand.length))
+                .slice(0, Math.min(positions.length, hand.length))
                 .map((card, i, _) => {
-                    const [x, y] = positions[props.playerIndex][i];
+                    const [x, y] = positions[i];
 
                     assert(card !== undefined, "No such card");
 
-                    if (props.playerIndex === 0 && card.color === CardColor.COLORLESS) {
+                    if (card.color === CardColor.COLORLESS) {
                         return <div key={i}>
                             <img
                                 src={`/cards/${card.id}.png`}
@@ -88,29 +53,33 @@ export default function Hand(props: {
                             />
 
                             <div className={"wish-buttons"} style={{top: y + 200, left: x}}>
-                                <button className={"wish-buttons-red"} onClick={() => props.moves.Play(i, CardColor.RED)}>r</button>
-                                <button className={"wish-buttons-green"} onClick={() => props.moves.Play(i, CardColor.GREEN)}>g</button>
-                                <button className={"wish-buttons-blue"} onClick={() => props.moves.Play(i, CardColor.BLUE)}>b</button>
-                                <button className={"wish-buttons-yellow"} onClick={() => props.moves.Play(i, CardColor.YELLOW)}>y</button>
+                                <button className={"wish-buttons-red"}
+                                        onClick={() => props.moves.Play(i, CardColor.RED)}>
+                                    r
+                                </button>
+                                <button className={"wish-buttons-green"}
+                                        onClick={() => props.moves.Play(i, CardColor.GREEN)}>
+                                    g
+                                </button>
+                                <button className={"wish-buttons-blue"}
+                                        onClick={() => props.moves.Play(i, CardColor.BLUE)}>
+                                    b
+                                </button>
+                                <button className={"wish-buttons-yellow"}
+                                        onClick={() => props.moves.Play(i, CardColor.YELLOW)}>
+                                    y
+                                </button>
                             </div>
                         </div>
                     }
 
-                    if (props.playerIndex === 0) {
-                        return <img
-                            src={`/cards/${card.id}.png`}
-                            style={{top: y, left: x}}
-                            alt={""}
-                            className={"card card-player-0"}
-                            key={i}
-                            onClick={() => props.moves.Play(i)}
-                        />
-                    }
-
-                    return <div
-                        className={`card card-player-${props.playerIndex}`}
+                    return <img
+                        src={`/cards/${card.id}.png`}
                         style={{top: y, left: x}}
-                        key={props.playerIndex + i}
+                        alt={""}
+                        className={"card card-player-0"}
+                        key={i}
+                        onClick={() => props.moves.Play(i)}
                     />
                 })
         }
