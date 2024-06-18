0e94613af0df27a87dcf20fdebd690b46a2e2346
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-06-18 10:48:43 +0200

    fix: playerID refers to the current client's player, not to the current player
    fix: playerID refers to the ID of the current client

--- src/lib/moves/draw.ts
+++ src/lib/moves/draw.ts
@@ -5,10 +5,10 @@ import {GameState, SerializableGameState} from "$/lib/api";
  * Draws a card from the deck and adds it to the current player's hand.
  * @move
  */
-export const Draw: Move<SerializableGameState> = ({G: g, events, playerID}) => {
+export const Draw: Move<SerializableGameState> = ({G: g, events, ctx }) => {
     const G = GameState.deserialize(g);
 
-    G.getPlayer(playerID).draw(G.deck);
+    G.getPlayer(ctx.currentPlayer).draw(G.deck);
 
     G.serialize(g);
     events.endTurn();
