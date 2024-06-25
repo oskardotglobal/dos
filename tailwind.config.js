7442b68a5094fae636e97b01e157d70b49475c7b
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-06-25 14:39:35 +0200

    fix: upgrade severely outdated packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
-module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+export default {
+  content: ["index.html","./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
     maxWidth: {
-        '3/5': '60%'
+        "3/5": "60%"
     },
     extend: {
       colors: {
-        'menu-gradient-end': '#6E7788',
-        'menu-gradient-transition': '#A2ACBD',
-        'menu-gradient-start': '#012A4A',
-        'primary-button': '#3E4756',
-        'primary-button-hover': '#465060',
-        'secondary-button': '#556176',
-        'secondary-button-hover': '#5b687f',
-        'enemy-deck': '#e6e6e6',
-        'player-deck': '#999da5'
+        "menu-gradient-end": "#6E7788",
+        "menu-gradient-transition": "#A2ACBD",
+        "menu-gradient-start": "#012A4A",
+        "primary-button": "#3E4756",
+        "primary-button-hover": "#465060",
+        "secondary-button": "#556176",
+        "secondary-button-hover": "#5b687f",
+        "enemy-deck": "#e6e6e6",
+        "player-deck": "#999da5"
       },
       fontFamily: {
-        'majorMonoDisplay': ['MajorMonoDisplay-Regular.ttf', 'serif'],
+        "majorMonoDisplay": ["MajorMonoDisplay-Regular.ttf", "serif"],
       },
     },
   },
