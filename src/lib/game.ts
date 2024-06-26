0c507b373f2ce253384a6d2c4f59e9981bde3e7d
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-06-26 14:19:00 +0200

    fix: mod before substraction

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -31,11 +31,11 @@ export const DosGame: Game<SerializableGameState> = {
             first: (_) => 0,
 
             // Get the next value of playOrderPos.
-            // This is called at the end of each turn.
+            // This is called at the end of each turn.x
             // The phase ends if this returns undefined.
             // Here, we use a ternary statement to get the next player depending on whether the current turn order is reversed
             // Note that we simply use `g` here, we do not need `G` since we only read from unserialized attributes anyway
-            next: ({ G: g, ctx }) => (ctx.playOrderPos + (g.turnOrderReversed ? -1 : 1)) % ctx.numPlayers - 1,
+            next: ({ G: g, ctx }) => (ctx.playOrderPos + (g.turnOrderReversed ? -1 : 1)) % (ctx.numPlayers - 1),
         }
     },
     endIf: ({ G: g, ctx }) => GameState.deserialize(g).endIf(ctx),
